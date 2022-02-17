c5fc802a4aaaab7a4fc6f3078262d16c772ef758
Amani Alhaw <amaniheritage@gmail.com>
2022-02-17 11:06:08 -0500

    Robot with fetched Data

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import SearchBox from "./SearchBox";
 import { Component } from "react";
-import { robots } from "./robots";
+
 import CardList from "./CardList";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
   }
@@ -17,19 +17,29 @@ class App extends Component {
     this.setState({ searchfield: event.target.value });
   };
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
   render() {
     const filteredRobots = this.state.robots.filter((robots) => {
       return robots.name
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1>***Robots***</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading....</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1>***Robots***</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
