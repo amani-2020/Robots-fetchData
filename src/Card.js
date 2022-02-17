d2dc49c0f20288c0d46e7e1097b68c818c7ea2d2
amani-2020 <61700092+amani-2020@users.noreply.github.com>
2022-02-17 17:16:26 -0500

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
   const { id, name, email } = props;
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src={`https://robohash.org/${id}+?200x200`} alt="robots" />
+      <img src={`https://robohash.org/${id}+?size=200x200`} alt="robots" />
       <div>
         <p>{name}</p>
         <p>{email}</p>
